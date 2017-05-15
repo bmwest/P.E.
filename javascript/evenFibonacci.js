0c5a681b0760d3fe7b15d1d49bb89f4a76eaa747
bmwest <brianawest.m@gmail.com>
2017-05-15 16:57:18 -0400

    adds missing semicolons

--- javascript/evenFibonacci.js
+++ javascript/evenFibonacci.js
@@ -2,16 +2,16 @@ let evenFibonacci = (num1, num2, num3) => {
   var sum = 0;
 
   while (num3 < 4000000) {
-    num3 = num1 + num2
+    num3 = num1 + num2;
   if (num3 % 2 == 0) {
-    sum += num3
-  }
-    num1 = num2
-    num2 = num3
-  }
-  return sum
-}
+    sum += num3;
+  };
+    num1 = num2;
+    num2 = num3;
+  };
+  return sum;
+};
 
-let e = evenFibonacci(0, 1, 0)
+let e = evenFibonacci(0, 1, 0);
 
-console.log(e)
+console.log(e);
--- javascript/recursiveFibonacci.js
+++ javascript/recursiveFibonacci.js
@@ -1,11 +1,12 @@
 let fibonacci = (num1, num2) => {
-  let num3 = num1 + num2
+  let num3 = num1 + num2;
+
   if (num3 >= 4000000) {
-    return
-  }
-  console.log(num3)
-  return fibonacci(num2, num3)
-}
+    return;
+  };
+  console.log(num3);
+  return fibonacci(num2, num3);
+};
 
 let r = fibonacci(0, 1);
 
